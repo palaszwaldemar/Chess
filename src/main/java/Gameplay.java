801f5c06e243e3a49251cd06a9dd426b79f82ddd
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-01-19 22:16:55 +0100

    initialized project and setup game engine

---
+++ src/main/java/Gameplay.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gameplay implements MouseListener {
+
+
+    public Gameplay() {
+    }
+
+    public void tick() {
+    }
+
+    public void render(Graphics g) {
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
+
+//podgląd aktualnego
+//postawienie aktualnego chowa podgląd
+//również aktywuje przycisk
+//również blokuje stawianie klocka
+//przycisk zatwierdzenia - naciśnięcie pokazuje podgląd kolejnego
+//przyleganie dróg - zezwala lub nie na położenie tile
