3d8367a5e37a5518351586c3d2f85aca00d4a527
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-01-27 14:50:43 +0100

    refactoring of chessboard display code

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -2,18 +2,26 @@ import java.awt.*;
 
 public class Board {
     private boolean filedIsBlack;
+    private final int HEIGHT_OF_FIELD = 100;
+    private final int WIDTH_OF_FIELD = 100;
+    private int x = 10;
+    private int y = 10;
 
     void render(Graphics g) {
-        for (int i = 0, y = 10; i < 8; i++, y += 100) {
-            for (int j = 0, x = 10; j < 8; j++, x += 100) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 if (filedIsBlack) {
-                    g.fillRect(x, y, 100, 100);
+                    g.fillRect(x, y, WIDTH_OF_FIELD, HEIGHT_OF_FIELD);
                 } else {
-                    g.drawRect(x, y, 100, 100);
+                    g.drawRect(x, y, WIDTH_OF_FIELD, HEIGHT_OF_FIELD);
                 }
                 filedIsBlack = !filedIsBlack;
+                x += 100;
             }
             filedIsBlack = !filedIsBlack;
+            y += 100;
+            x = 10;
         }
+        y = 10;
     }
 }
