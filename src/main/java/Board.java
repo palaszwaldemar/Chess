bd51b55de313c9ba6f828c2552ac9c403ced977c
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-01-27 17:54:09 +0100

    refactoring of chessboard display code

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,24 +1,25 @@
 import java.awt.*;
 
 public class Board {
-    private boolean filedIsBlack;
-    private final int HEIGHT_OF_FIELD = 100;
+    private final int HEIGHT_OF_FIELD = 100;// CHECK : 27.01.2023 czy te pola powinienem umieścić w metodzie render?
     private final int WIDTH_OF_FIELD = 100;
     private int x = 10;
     private int y = 10;
 
-    void render(Graphics g) {
+    void render(Graphics g) {// CHECK : 27.01.2023 render odpala się na okrągło?
+        displayChessBoard(g);
+    }
+
+    private void displayChessBoard(Graphics g) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (filedIsBlack) {
+                if (((x + y) / 100) % 2 != 0 ) {
                     g.fillRect(x, y, WIDTH_OF_FIELD, HEIGHT_OF_FIELD);
                 } else {
                     g.drawRect(x, y, WIDTH_OF_FIELD, HEIGHT_OF_FIELD);
                 }
-                filedIsBlack = !filedIsBlack;
                 x += 100;
             }
-            filedIsBlack = !filedIsBlack;
             y += 100;
             x = 10;
         }
