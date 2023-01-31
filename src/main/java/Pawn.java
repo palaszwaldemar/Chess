cc5d71edeb71f936c5c3f6acf37a30097fc9a74a
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-01-31 21:08:00 +0100

    change location of the pictures of pawns

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -10,11 +10,10 @@ public class Pawn {
     private BufferedImage bufferedImage;
 
     public Pawn(char letter, int number, NamesOfPawns namesOfPawns) {
-        String name = namesOfPawns.getName();
         this.letter = letter;
         this.number = number;
         try {
-            this.bufferedImage = ImageIO.read(Objects.requireNonNull(Pawn.class.getResource("/chess pieces/pawns/" + name + ".png")));
+            this.bufferedImage = ImageIO.read(Objects.requireNonNull(Pawn.class.getResource("/chess pieces/" + namesOfPawns.getName() + ".png")));
 
         } catch (IllegalArgumentException | IOException e) {
             e.printStackTrace();// CHECK : 31.01.2023 czemu ta metoda ?
