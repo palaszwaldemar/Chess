cae0268e4475b9132805a9506063ff589262078c
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-02-03 17:49:09 +0100

    resize chess pieces when resizing the chessboard

--- src/main/java/ChessPiece.java
+++ src/main/java/ChessPiece.java
@@ -7,13 +7,14 @@ import java.util.Objects;
 public class ChessPiece {
     private final int x;
     private final int y;
-    private BufferedImage bufferedImage;
+    private Image image;
 
     public ChessPiece(int x, int y, NamesOfPawns namesOfPawns) {
         this.x = x;
         this.y = y;
         try {
-            this.bufferedImage = ImageIO.read(Objects.requireNonNull(ChessPiece.class.getResource("/chess pieces/" + namesOfPawns.getName() + ".png")));
+            BufferedImage bufferedImage = ImageIO.read(Objects.requireNonNull(ChessPiece.class.getResource("/chess pieces/" + namesOfPawns.getName() + ".png")));
+            this.image = bufferedImage.getScaledInstance(Board.getSIZE_OF_FIELD(), Board.getSIZE_OF_FIELD(), Image.SCALE_DEFAULT);
 
         } catch (IllegalArgumentException | IOException e) {
             e.printStackTrace();
@@ -21,6 +22,6 @@ public class ChessPiece {
     }
 
     void render(Graphics g) {
-        g.drawImage(bufferedImage, x * Board.getSIZE_OF_FIELD(), y * Board.getSIZE_OF_FIELD(), null);
+        g.drawImage(image, x * Board.getSIZE_OF_FIELD(), y * Board.getSIZE_OF_FIELD(), null);
     }
 }
